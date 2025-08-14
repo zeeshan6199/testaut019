34fc482ca2a53817e16b7abcd65fae667c1ba53b
zeeshanaslam <zeeshanaslam7999@gmail.com>
2025-08-15 00:49:41 +0500

    added one comment

--- tests/test_loginaction.py
+++ tests/test_loginaction.py
@@ -3,6 +3,7 @@ from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 def test_loginaction(postive_logged_in,page):
     #visit page and logging in
+    #justtesting something
     LoginPage(postive_logged_in)
     prodpage=ProductsPage(page)
     expect(prodpage.verifytitle()).to_be_visible()
